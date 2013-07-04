7171fbf93805a6f9cfacbb7b2af8ce3d6b645526
German Tischler <gt1@sanger.ac.uk>
2013-07-04 15:38:25 +0100

    refactor bamzztoname

---
+++ src/biobambam/zzToName.cpp
@@ -0,0 +1,32 @@
+/**
+    bambam
+    Copyright (C) 2009-2013 German Tischler
+    Copyright (C) 2011-2013 Genome Research Limited
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+#include <biobambam/zzToName.hpp>
+
+bool zzToRank(libmaus::bambam::BamAlignment & algn, libmaus::bambam::BamAuxFilterVector const & zzbafv)
+{
+	uint64_t const rank = algn.getRank("zz");
+	algn.filterOutAux(zzbafv);
+	
+	std::string const newname = 
+		libmaus::util::NumberSerialisation::formatNumber(rank,0) + "_" + algn.getName();
+		
+	algn.replaceName(newname.begin(),newname.size());
+
+	return true;
+}
