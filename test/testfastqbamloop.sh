60ebc170acf8d6bafbcdd61e05fca00e519db11b
German Tischler <gt1@sanger.ac.uk>
2014-04-30 09:29:28 +0100

    update mini test for fastqtobam/bamtofastq loop

--- test/testfastqbamloop.sh
+++ test/testfastqbamloop.sh
@@ -1,18 +1,45 @@
 #! /bin/bash
-../src/fastqtobam precollated.fq | ../src/bamtofastq | cmp precollated.fq
+function precollated_fq
+{
+cat <<EOF
+@A/1
+ACGTACGT
++
+HGFEDCBA
+@A/2
+GTCAGTCA
++
+GHGFEDCB
+EOF
+}
 
-# copy pipe return status array
-PIPESTAT=( ${PIPESTATUS[*]} )
+function runopts
+{
+	../src/fastqtobam $* <(precollated_fq) | ../src/bamtofastq | cmp <(precollated_fq)
 
-if [ ${PIPESTAT[0]} -ne 0 ] ; then
-	echo 'fastqtobam failed'
-	exit 1
-elif [ ${PIPESTAT[1]} -ne 0 ] ; then
-	echo 'bamtofastq failed'
-	exit 1
-elif [ ${PIPESTAT[2]} -ne 0 ] ; then
-	echo 'cmp failed'
-	exit 1
-fi
+	# copy pipe return status array
+	PIPESTAT=( ${PIPESTATUS[*]} )
 
-exit 0
+	if [ ${PIPESTAT[0]} -ne 0 ] ; then
+		echo 'fastqtobam failed'
+		return 1
+	elif [ ${PIPESTAT[1]} -ne 0 ] ; then
+		echo 'bamtofastq failed'
+		return 1
+	elif [ ${PIPESTAT[2]} -ne 0 ] ; then
+		echo 'cmp failed'
+		return 1
+	else
+		return 0
+	fi
+}
+
+runopts ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
+runopts namescheme=generic ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
+echo "This is expected to fail:"
+runopts namescheme=c18s ; R=$? ; if [ ${R} -eq 0 ] ; then exit 1 ; fi
+echo "This is expected to fail:"
+runopts namescheme=c18pe ; R=$? ; if [ ${R} -eq 0 ] ; then exit 1 ; fi
+runopts namescheme=pairedfiles ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
+
+exit ${R}
