af340d1471af921f535643aba1b64f7a766d5c18
German Tischler <gt1@sanger.ac.uk>
2014-05-01 14:02:18 +0100

    add tests in fastq -> bam -> fastq test loop

--- test/testfastqbamloop.sh
+++ test/testfastqbamloop.sh
@@ -1,21 +1,53 @@
 #! /bin/bash
-function precollated_fq
+function precollated_fq_1
 {
 cat <<EOF
 @A/1
 ACGTACGT
 +
 HGFEDCBA
+EOF
+}
+
+function precollated_fq_2
+{
+cat <<EOF
 @A/2
 GTCAGTCA
 +
 GHGFEDCB
 EOF
 }
+function precollated_fq
+{
+	precollated_fq_1
+	precollated_fq_2
+}
 
 function runopts
 {
 	../src/fastqtobam $* <(precollated_fq) | ../src/bamtofastq | cmp <(precollated_fq)
+
+	# copy pipe return status array
+	PIPESTAT=( ${PIPESTATUS[*]} )
+
+	if [ ${PIPESTAT[0]} -ne 0 ] ; then
+		echo 'fastqtobam failed'
+		return 1
+	elif [ ${PIPESTAT[1]} -ne 0 ] ; then
+		echo 'bamtofastq failed'
+		return 1
+	elif [ ${PIPESTAT[2]} -ne 0 ] ; then
+		echo 'cmp failed'
+		return 1
+	else
+		return 0
+	fi
+}
+
+function runopts2
+{
+	../src/fastqtobam $* <(precollated_fq_1) <(precollated_fq_2) | ../src/bamtofastq | cmp <(precollated_fq)
 
 	# copy pipe return status array
 	PIPESTAT=( ${PIPESTATUS[*]} )
@@ -41,5 +73,13 @@ runopts namescheme=c18s ; R=$? ; if [ ${R} -eq 0 ] ; then exit 1 ; fi
 echo "This is expected to fail:"
 runopts namescheme=c18pe ; R=$? ; if [ ${R} -eq 0 ] ; then exit 1 ; fi
 runopts namescheme=pairedfiles ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
+
+runopts2 ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
+runopts2 namescheme=generic ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
+echo "This is expected to fail:"
+runopts2 namescheme=c18s ; R=$? ; if [ ${R} -eq 0 ] ; then exit 1 ; fi
+echo "This is expected to fail:"
+runopts2 namescheme=c18pe ; R=$? ; if [ ${R} -eq 0 ] ; then exit 1 ; fi
+runopts2 namescheme=pairedfiles ; R=$? ; if [ ${R} -ne 0 ] ; then exit ${R} ; fi
 
 exit ${R}
