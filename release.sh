a7b2c49f643dba5e54915593dfec44f2de4451d8
German Tischler <gt1@sanger.ac.uk>
2014-05-08 10:40:29 +0100

    add missing file in release branches

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ DATE=`date +"%Y%m%d%H%M%S"`
 RELEASE=${VERSION}-release-${DATE}
 PATH=/software/hpag/autotools/bin:${PATH} autoreconf -i -f
 git checkout -b ${RELEASE}-branch master
-ADDFILES="INSTALL Makefile.in aclocal.m4 autom4te.cache compile config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4 missing src/Makefile.in test/Makefile.in"
+ADDFILES="INSTALL Makefile.in aclocal.m4 autom4te.cache compile config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4 missing src/Makefile.in test/Makefile.in test-driver"
 mv .gitignore .gitignore_
 git add ${ADDFILES}
 git commit -m "Release ${RELEASE}"
