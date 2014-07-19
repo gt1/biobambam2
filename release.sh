49c08dca5562f03d9ed83fa6711d26d7401bf78b
German Tischler <gt1@sanger.ac.uk>
2014-07-19 08:32:19 +0100

    update release script

--- release.sh
+++ release.sh
@@ -1,11 +1,32 @@
 #! /bin/bash
+
+# update branches
+git checkout experimental
+git pull
+git checkout experimental-debian
+git pull
+git checkout debian
+git pull
+git checkout master
+git pull
+
+# merge
+git checkout master
+git merge experimental
+git push
+
+git checkout debian
+git merge experimental-debian
+git push
+
+# add release tag/branch
 git checkout master
 VERSION=`grep <configure.ac "AC_INIT" | perl -p -e "s/.*AC_INIT\(//" | awk -F ',' '{print $2}'`
 DATE=`date +"%Y%m%d%H%M%S"`
 RELEASE=${VERSION}-release-${DATE}
-PATH=/software/hpag/autotools/bin:${PATH} autoreconf -i -f
 git checkout -b ${RELEASE}-branch master
-ADDFILES="INSTALL Makefile.in aclocal.m4 autom4te.cache compile config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4 missing src/Makefile.in test/Makefile.in test-driver"
+PATH=/software/hpag/autotools/bin:${PATH} autoreconf -i -f
+ADDFILES="INSTALL Makefile.in aclocal.m4 autom4te.cache compile config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4 missing src/Makefile.in"
 mv .gitignore .gitignore_
 git add ${ADDFILES}
 git commit -m "Release ${RELEASE}"
