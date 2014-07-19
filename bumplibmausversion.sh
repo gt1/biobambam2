4f087c695c33753df11145664bf9b123d1117531
German Tischler <gt1@sanger.ac.uk>
2014-07-19 08:24:37 +0100

    add libmaus version bumping script

---
+++ bumplibmausversion.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+LIBMAUSVERSION=$1
+
+if [ -z ${LIBMAUSVERSION} ] ; then
+	exit 1
+fi
+
+sed -i -e "s/libmaus >= [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/libmaus >= ${LIBMAUSVERSION}/" configure.ac
+git add configure.ac
+git commit -m "bump libmaus version"
+git push
+
+git checkout experimental-debian
+sed -i -e "s/libmaus-dev (>= [0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*)/libmaus-dev (>= ${LIBMAUSVERSION})/" debian/control
+git add debian/control
+git commit -m "bump libmaus version"
+git push
+
+git checkout experimental
