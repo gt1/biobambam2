995429fe906b49a9ce00c71d22aff78035745c21
German Tischler <gt1@sanger.ac.uk>
2015-04-17 15:40:23 +0100

    fix rank stripping for single end reads

--- src/biobambam2/Strip12.cpp
+++ src/biobambam2/Strip12.cpp
@@ -22,18 +22,21 @@ bool strip12(libmaus2::bambam::BamAlignment & algn)
 {
 	char const * name = algn.getName();
 	
+	// look for first underscore symbol
 	char const * u1 = name;
 	
 	while ( *u1 && *u1 != '_' )
 		++u1;
-					
+	
+	// no underscore				
 	if ( ! *u1 )
 		return true;
 	else
 	{
+		// check whether all symbols before underscore are digits
 		bool ok = true;
 		uint64_t ranka = 0;
-			
+
 		for ( char const * t1 = name; t1 != u1; ++t1 )
 		{	
 			ranka *= 10;
@@ -41,14 +44,17 @@ bool strip12(libmaus2::bambam::BamAlignment & algn)
 			ok = ok && isdigit(*t1);
 		}
 
-		int const read1 = algn.isRead1() ? 1 : 0;
-		int const read2 = algn.isRead2() ? 1 : 0;
-			
-		if ( (read1+read2 != 1) || (!ok) )
+		// no number found?
+		if ( ! ok )
 		{
 			return true;
 		}
-		else
+
+		int const read1 = algn.isRead1() ? 1 : 0;
+		int const read2 = algn.isRead2() ? 1 : 0;
+
+		// single end or (read1 or read2 of pair)
+		if ( (! algn.isPaired()) || (algn.isPaired() && (read1+read2==1)) )
 		{
 			std::ostringstream upnamestr;
 
@@ -58,7 +64,11 @@ bool strip12(libmaus2::bambam::BamAlignment & algn)
 				
 			algn.replaceName(upname.begin(),upname.size());
 			
-			return true;
+			return true;		
+		}
+		else
+		{
+			return true;		
 		}
 	}
 
