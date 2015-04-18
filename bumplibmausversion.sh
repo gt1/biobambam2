2d6d7ef4ae0456b9730a101172e1bb3c247c1093
German Tischler <gt1@sanger.ac.uk>
2015-04-19 00:14:00 +0100

    update libmaus2seqchksumsfactory version when bumping libmaus version

--- bumplibmausversion.sh
+++ bumplibmausversion.sh
@@ -8,6 +8,7 @@ fi
 sed -i -e "s/libmaus2 >= [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/libmaus2 >= ${LIBMAUS2VERSION}/" configure.ac
 sed -i -e "s/libmaus2digests >= [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/libmaus2digests >= ${LIBMAUS2VERSION}/" configure.ac
 sed -i -e "s/libmaus2irods >= [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/libmaus2irods >= ${LIBMAUS2VERSION}/" configure.ac
+sed -i -e "s/libmaus2seqchksumsfactory >= [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/libmaus2seqchksumsfactory >= ${LIBMAUS2VERSION}/" configure.ac
 git add configure.ac
 git commit -m "bump libmaus2 version"
 git push
