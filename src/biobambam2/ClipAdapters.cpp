15f3de89ab51b7661314bff0676b75ebaeb367ec
Andrew Whitwham <whitwham@users.noreply.github.com>
2015-09-22 14:55:03 +0100

    Added standard copyright notice and minor code change.

--- src/biobambam2/ClipAdapters.cpp
+++ src/biobambam2/ClipAdapters.cpp
@@ -1,4 +1,52 @@
+/**
+    bambam
+    Copyright (C) 2013 German Tischler
+    Copyright (C) 2013, 2015 Genome Research Limited
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ----------------------------------------------------------------------
+    
+    Cigar type code taken from htslib/sam.h under the MIT/Expat License
+    as follows:
+    
+     Copyright (C) 2008, 2009, 2013-2014 Genome Research Ltd.
+     Copyright (C) 2010, 2012, 2013 Broad Institute.
+
+     Author: Heng Li <lh3@sanger.ac.uk>
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE    
+**/
 #include <biobambam2/ClipAdapters.hpp>
+
+/* Start of htslib code */
 
 #define BAM_CIGAR_TYPE  0x3C1A7
 #define bam_cigar_type(o) (BAM_CIGAR_TYPE>>((o)<<1)&3)
@@ -25,6 +73,8 @@
     BAM_CDIFF       1      1
     BAM_CBACK       0      0
     --------------------------------
+    
+    End of htslib code.
 */
     
 
@@ -107,7 +157,7 @@ bool clipAdapters(
 				    	cigop[index++].second -= left;
 				}
 				
- 				cigop[index] = libmaus2::bambam::cigar_operation(5,aclip + hardclip);
+ 				cigop[index] = libmaus2::bambam::cigar_operation(libmaus2::bambam::BamFlagBase::LIBMAUS2_BAMBAM_CHARD_CLIP, aclip + hardclip);
 				
 			    	if ( numcigop > index + 1 ) 
 				    	cigop.resize(index + 1);
